55967e92e64d5e3360e8d826408c0660fef487d7
David Amos - (on Fedora) <wove5@outlook.com>
2023-09-26 17:42:01 +1000

    test: reduce expiration time for testing & debugging

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -5,7 +5,7 @@ export const signJwt = (payload: Object, options: SignOptions = {}) => {
   return jwt.sign(payload, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
-    expiresIn: '5m',
+    expiresIn: '2m',
   });
 };
 
