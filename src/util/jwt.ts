ac816d91f8bcd08d917d8212a2120b59effda153
David Amos - (on Fedora) <wove5@outlook.com>
2023-09-26 13:41:37 +1000

    chore: more logger info msg's in jwt.ts to debug server

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -4,12 +4,14 @@ import logger from './logger';
 export const signJwt = (payload: Object, options: SignOptions = {}) => {
   const privateKey = process.env.accessTokenPrivateKey as string;
   logger.info(`⚡️[server]: privateKey is: ${privateKey}.`);
-  logger.info(`⚡️[server]: payload = ${payload}`);
-  return jwt.sign(payload, privateKey, {
+  logger.info(`⚡️[server]: payload object = ${Object.entries(payload)}`);
+  const result = jwt.sign(payload, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
     expiresIn: '5m',
   });
+  logger.info(`⚡️[server]: result = ${result} `);
+  return result;
 };
 
 export const verifyJwt = <T>(token: string): T | null => {
